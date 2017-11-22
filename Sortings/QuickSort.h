e4e83d4d259271264e9a792b1007eebc7776f3da
IDragnev <iddragnev@gmail.com>
2017-11-22 15:02:19 +0200

    Leaving the median-of-3 out of the partition function

--- Sortings/QuickSort.h
+++ Sortings/QuickSort.h
@@ -25,57 +25,49 @@ void quickSort(DynamicArray<T>& arr, int beg, int end)
 		}
 		else 
 		{
-			//get the location of the pivot
-			int middle = partition(arr, beg, end);
+			// median-of-3 with beg, end and middle
+			int middle = size / 2;
+			swapif(arr[beg], arr[middle]);
+			swapif(arr[beg], arr[end]);
+			swapif(arr[middle], arr[end]);
+		
+			//put the pivot in the penult position
+			//because arr[end] is bigger than it
+			//and there is no need to include it in the partitioning
+			std::swap(arr[middle], arr[end - 1]);
+
+			//partition without the most left and right elements
+			int pivot = partition(arr, beg + 1, end - 1);
 
 			//sort left part recursively
-			quickSort(arr, beg, middle - 1);
+			quickSort(arr, beg, pivot - 1);
 
 			//sort right part recursively
-			quickSort(arr, middle + 1, end);
+			quickSort(arr, pivot + 1, end);
 		}
 	}
 }
 
 
 
+
+	
 //
 //the auxiliary operation
 //
 template <typename T>
-int partition(DynamicArray<T> &arr, int beg, int end)
+int partition(DynamicArray<T>& arr, int beg, int end)
 {
-	int size = end - beg + 1;
-
-	assert(size >= 3);
-
-	// median-of-3 with beg, end and middle
-	int middle = size / 2;
-	swapif(arr[beg], arr[middle]);
-	swapif(arr[beg], arr[end]);
-	swapif(arr[middle], arr[end]);
-
-	//put the pivot in the penult position
-	//because arr[end] is bigger than it
-	//and there is no need to include it in the partitioning
-	std::swap(arr[middle], arr[end - 1]);
-
 	//the pivot
-	T &pivot = arr[end - 1];
+	T &pivot = arr[end];
 
-	//now partition the array without 
-	//including the most left and right elements
-	//because they were already sorted with pivot
-	int left = beg + 1;   //++beg;
-	int right = end - 1;  //--end;
+	int i = beg - 1;
 
-	//the [ x <= pivot ] part
-	int i = left - 1;
-	
-	//from the most left to the pivot
-	for (int j = left; j < right; ++j)
+	//from the first element to the pivot
+	for (int j = beg; j < end; ++j)
 	{
-		//if x <= pivot
+		//if the current element is not greater
+		//than the pivot
 		if (arr[j] <= pivot)
 		{
 			//increment left part
@@ -89,8 +81,8 @@ int partition(DynamicArray<T> &arr, int beg, int end)
 	}
 
 	//put the pivot between the two parts
-	std::swap(arr[i + 1], arr[right]);
+	std::swap(arr[i + 1], arr[end]);
 
 	//and return its location
-	return (i + 1 != right) ? (i + 1) : (left + right) / 2;
+	return (i + 1 != end) ? (i + 1) : (beg + end) / 2;
 }
