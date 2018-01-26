45766c1e71b87542a49b69b94a83f22191c04db2
IDragnev <iddragnev@gmail.com>
2018-01-26 17:40:44 +0200

    Adding reverseList and swapWithNext functions for lists

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -4,6 +4,63 @@
 #include "ForwardList.h"
 
 
+
+//
+//reverse a linked list
+//
+template <typename T>
+void reverseList(Node<T>* &first)
+{
+	//if less than two, do nothing
+	if (!first || !first->next) return;
+
+	//starting from the second node in the list
+	Node<T>* previous = first;
+	Node<T>* current = first->next;
+
+	//while in the list
+	while (current)
+	{
+		//save the address of the next
+		Node<T>* next = current->next;
+
+		//reverse link
+		current->next = previous;
+
+		//move previous and current forward
+		previous = current;
+		current = next;
+	}
+
+	first->next = nullptr;
+	first = previous;
+}
+
+
+//
+//swap a node with its successor
+//
+template <typename T>
+void swapWithNext(Node<T>* &node)
+{
+	//if empty or no successor, do nothing
+	if (!node || !node->next) return;
+
+	Node<T>* sucessor = node->next;
+
+	//unlink node with successor
+	node->next = successor->next;
+
+	//put successor before node
+	successor->next = node;
+
+	//set node point to the new beginning
+	node = successor;
+}
+
+
+
+
 int main()
 {
 	try
