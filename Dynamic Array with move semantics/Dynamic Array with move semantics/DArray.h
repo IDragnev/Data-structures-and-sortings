b1b78dbc79d57438439193138e5817db7095330a
IDragnev <iddragnev@gmail.com>
2018-02-15 23:43:04 +0200

    Starting DArray class (with move sem.)

---
+++ Dynamic Array with move semantics/Dynamic Array with move semantics/DArray.h
@@ -0,0 +1,72 @@
+#ifndef __D_ARRAY_M_SEMANT_H_INCLDUED__
+#define __D_ARRAY_M_SEMANT_H_INCLDUED__
+
+#include <assert.h>
+#include <utility>
+#include <stdexcept>
+
+template <typename T>
+class DArray
+{
+public:
+	DArray();
+	explicit DArray(int size, int count = 0);
+	DArray(DArray<T>&&);
+	DArray(const DArray<T>&);
+	virtual ~DArray();
+
+	DArray<T>& operator=(DArray<T>&&);
+	DArray<T>& operator=(const DArray<T>&);
+
+	void append(DArray<T>&&);
+	void append(const DArray<T>&);
+
+public:
+	int getSize()const;
+	int getCount()const;
+	bool isEmpty()const;
+
+	void makeEmpty();
+	void ensureSize(int size);
+
+public:
+	virtual void add(T&&);
+	virtual void add(const T&);
+	virtual void remove(int position);
+	virtual int find(const T&)const;
+
+public:
+	void addAt(int, const T&);
+	void addAt(int, T&&);
+
+public:
+	T& operator[](int position);
+	const T& operator[](int position)const;
+
+private:
+	int count;
+	int size;
+	T* data;
+
+protected:
+	void setSize(int);
+	void setCount(int);
+
+	void checkSpace();
+
+	void shiftLeft(int beg, int end);
+	void shiftRight(int beg, int end);
+
+	T* data();
+	const T* data()const;
+
+private:
+	void copy(const DArray<T>&);
+	void resize(int);
+	void clear();
+	void null();
+	void directInit(T*, int count, int size);
+};
+
+#include "DArrayImpl.hpp"
+#endif //__D_ARRAY_M_SEMANT_H_INCLDUED__
